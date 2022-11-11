1d635805268e6ba3292b864c7e51a6430eb6eb39
raphaelgadino <raphael.costa@db1.com.br>
2022-11-11 15:26:42 -0300

    feat: style navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import ReactLogo from '../imgs/reactjs-icon.svg';
 
 export default function Navbar(){
     return (
-        <h1>Navbar goes herre</h1>
+        <nav className='navbar'>
+            <div className='navbar-logo'>
+                <img src={ReactLogo} alt="reactjs logo"/>
+                <h1>ReactJs</h1>
+            </div>
+            <h2>1º Class - Static Pages</h2>
+        </nav>
     )
 }
