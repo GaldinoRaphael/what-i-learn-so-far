7009f6f4aaac381e7eeab49d09952225deffe78d
raphaelgadino <raphael.costa@db1.com.br>
2022-11-14 10:01:42 -0300

    feat: main section

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Main(){
     return (
-        <h1>Main goes herre</h1>
+        <main className='main'>
+            <h1 className='main-title'>What I learned so far</h1>
+            <ul className='main-list'>
+                <li className='main-list--item' >Function components</li>
+                <li className='main-list--item'>Basic project configurations</li>
+                <li className='main-list--item'>JSX</li>
+            </ul>
+        </main>
     )
 }
